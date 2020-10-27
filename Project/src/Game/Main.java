daf31d3baa41289b8b3fb5538a17edd0978d7b07
onlyfortok <onlyfortok@gmail.com>
2020-10-27 13:25:10 -0700

    update1

--- Project/src/Game/Main.java
+++ Project/src/Game/Main.java
@@ -3,5 +3,7 @@ package Game;
 public class Main {
     public static void main(String[] args){
         System.out.println("this is working");
+        System.out.println("this is working");
+
     }
 }
